6ef6f5339d67ad67e4b413ee1eee775ff5cbf16a
GayMer <40738180+DarkPyDoor@users.noreply.github.com>
2023-11-11 21:42:35 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import HTMLResponse
+import keras_ocr
+import matplotlib.pyplot as plt
+import os
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
+# Загрузка модели OCR
+my_model = keras.models.load_model('captcha_ocr_model.h5')
+pipeline = keras_ocr.pipeline.Pipeline(detector=my_model)
+
+@app.get('/')
+def home():
+    return HTMLResponse(content=open('index.html').read(), status_code=200)
+
+@app.post('/process')
+async def process(image: UploadFile = File(...)):
+    # Получение загруженного изображения
+    image_path = 'static/' + image.filename
+    with open(image_path, 'wb') as f:
+        f.write(image.file.read())
+
+    # Обработка изображения с помощью OCR
+    images = [keras_ocr.tools.read(image_path)]
+    prediction_groups = pipeline.recognize(images)
+
+    # Удаление временного изображения
+    os.remove(image_path)
+
+    # Возвращение результатов OCR в шаблон HTML
+    return HTMLResponse(content=open('result.html').read(), status_code=200, 
+                        media_type='text/html', context={'request': request, 'predictions': prediction_groups[0]})
